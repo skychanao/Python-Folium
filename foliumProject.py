2a15f0bb9a9c02df2c5a4eba5e81a42361bdcf7c
skychanao <skyforunivesity@gmail.com>
2026-02-05 22:15:28 +0100

    added munich json, created request geojson files

--- foliumProject.py
+++ foliumProject.py
@@ -1,5 +1,22 @@
 import folium
+import requests
 
-m = folium.Map(location=(45.5236, -122.6750))
 
-m
+m = folium.Map(location=(48.1351, 11.5820),zoom_start = 10)
+
+folium.Marker(
+    location=[48.140316111464784, 11.559727592980297],
+    tooltip = "Munich Hbf",
+    popup = "Central Station of munich",
+    icon=folium.Icon(icon="cloud")
+).add_to(m)
+
+geojson_data = requests.get(
+    "https://github.com/isellsoap/deutschlandGeoJSON/blob/main/4_kreise/3_mittel.geo.json"
+)
+
+folium.GeoJson(geojson_data, name="hello world").add_to(m)
+
+folium.LayerControl().add_to(m)
+
+m.save('map.html')
